9fc5695c3e8df8b7fe34133aa7125a60ee407bf5
minime556 <markuss7kalnins@gmail.com>
2025-03-31 14:33:34 +0300

    Metodē ieviests cikls

--- src/git_kaulins/Kaulins.java
+++ src/git_kaulins/Kaulins.java
@@ -4,14 +4,17 @@ import java.util.Random;
 
 public class Kaulins {
 
-	static void mestKaulinu() {
+	static void mestKaulinu(int reizes) {
 		int skaitlis;
 		Random rand = new Random();
-		skaitlis = rand.nextInt(6)+1;
+		for(int i=0; i<reizes; i++) {
+			skaitlis = rand.nextInt(6)+1;
+		
 		System.out.println("Uzkrita skaitlis: "+skaitlis);
+		}
 	}
 		public static void main(String[] args) {
-			mestKaulinu();
+			mestKaulinu(3);
 	}
 
 }
