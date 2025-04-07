e66eb0775e3cc4db4a395fc281d1a74867f493ab
Krapauskis <Arturs.Krapauskis@sk.lvt.lv>
2025-04-07 11:57:29 +0300

    Lielākā skaitļa meklēšana

--- src/git_kaulins/Kaulins.java
+++ src/git_kaulins/Kaulins.java
@@ -5,16 +5,18 @@ import java.util.Scanner;
 
 public class Kaulins {
 
-	static void mestKaulinu(int reizes) {
-		int skaitlis;
+	static int mestKaulinu(int reizes) {
+		int lielakais = 0, skaitlis;
 		Random rand = new Random();
 		for(int i=0; i<reizes; i++) {
 			skaitlis = rand.nextInt(6)+1;
-		
+			lielakais = lielakais(lielakais, skaitlis);
 		System.out.println("Uzkrita skaitlis: "+skaitlis);
 		}
+		return lielakais;
 	}
 		public static void main(String[] args) {
+			int lielakais;
 			Scanner scan = new Scanner(System.in);
 			int reizes;
 			do {
@@ -22,8 +24,15 @@ public class Kaulins {
 			reizes = scan.nextInt();
 			}while(reizes<1);
 			scan.close();
-			mestKaulinu(reizes);
-
+			lielakais = mestKaulinu(reizes);
+			System.out.println("Lielākais skaitlis ir "+lielakais);
 	}
+		static int lielakais(int lielakais, int skaitlis) {
+			if (lielakais > skaitlis) {
+				return lielakais;
+			} else {
+				return skaitlis;	
+			}
+		}
 
 }
